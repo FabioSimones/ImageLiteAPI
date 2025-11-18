f69fbbad4fae13309999a0f6780d9238a856ab1f
FabioSimones <simones.fabio1994@gmail.com>
2025-11-18 17:34:06 -0300

    Projeto criado.

---
+++ src/test/java/com/devfabiosimones/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfabiosimones.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
