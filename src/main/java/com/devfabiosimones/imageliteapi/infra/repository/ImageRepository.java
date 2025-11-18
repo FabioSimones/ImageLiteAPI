29254a2662fff7bf3a9125789992fe30da472499
FabioSimones <simones.fabio1994@gmail.com>
2025-11-18 18:51:21 -0300

    Configurando ImageRepository.

---
+++ src/main/java/com/devfabiosimones/imageliteapi/infra/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.devfabiosimones.imageliteapi.infra.repository;
+
+import com.devfabiosimones.imageliteapi.domain.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, String> {
+}
