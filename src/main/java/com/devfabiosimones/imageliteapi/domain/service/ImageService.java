e74b7157fec0d4de860d1fc1726f90907a52a840
FabioSimones <simones.fabio1994@gmail.com>
2025-11-19 18:10:25 -0300

    Criando URL para acesso de Imagem para renderização.

--- src/main/java/com/devfabiosimones/imageliteapi/domain/service/ImageService.java
+++ src/main/java/com/devfabiosimones/imageliteapi/domain/service/ImageService.java
@@ -2,7 +2,10 @@ package com.devfabiosimones.imageliteapi.domain.service;
 
 import com.devfabiosimones.imageliteapi.domain.entity.Image;
 
+import java.util.Optional;
+
 public interface ImageService {
 
     Image save(Image image);
+    Optional<Image> getById(String id);
 }
