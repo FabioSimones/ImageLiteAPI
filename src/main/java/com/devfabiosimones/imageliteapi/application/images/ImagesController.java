1a6d8d91afe64865c93a9e9cce34cec1887fa7bf
FabioSimones <simones.fabio1994@gmail.com>
2025-11-19 17:53:24 -0300

    Criando URL da imagem e configurando tamanho de Upload.

--- src/main/java/com/devfabiosimones/imageliteapi/application/images/ImagesController.java
+++ src/main/java/com/devfabiosimones/imageliteapi/application/images/ImagesController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,6 +25,7 @@ import java.util.List;
 public class ImagesController {
 
     private final ImageService service;
+    private final ImageMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
@@ -34,18 +37,17 @@ public class ImagesController {
         log.info("Content Type: {}", file.getContentType());
         log.info("Media Type: {}", MediaType.valueOf(file.getContentType()));
 
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imageURI = buildImageUrl(savedImage);
 
 
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(", ", tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        return ResponseEntity.created(imageURI).build();
+    }
 
-        service.save(image);
-
-        return ResponseEntity.ok().build();
+    private URI buildImageUrl(Image image) {
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder.fromCurrentRequest()
+                .path(imagePath).build().toUri();
     }
 }
